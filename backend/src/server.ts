4650633b805bdb549ece0faa49e7c54fd7f1dd2f
Tyandrer Boldt <tyandrer.boldt@gmail.com>
2020-10-14 09:21:09 -0300

    Views and render images on browser

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import './database/connection';
+import path from 'path';
 
 import routes from './routes';
 
@@ -7,5 +8,6 @@ const app = express();
 
 app.use(express.json());
 app.use(routes);
+app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')))
 
 app.listen(3333);
