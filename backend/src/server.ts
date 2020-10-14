938ad8bb60c6d4d0885b06652596ecbb27e382ac
Tyandrer Boldt <tyandrer.boldt@gmail.com>
2020-10-14 08:24:28 -0300

    Add GET resources on Orphanages

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,41 +1,11 @@
 import express from 'express';
-
 import './database/connection';
-import { getRepository } from 'typeorm';
-import Orphanage from './models/Orphanage';
+
+import routes from './routes';
 
 const app = express();
 
 app.use(express.json());
-
-/* ROUTES */
-app.post('/orphanages', async (request, response) => {
-  const {
-    name,
-    longitude,
-    latitude,
-    about,
-    instructions,
-    opening_hours,
-    open_on_weekends
-  } = request.body;
-
-  const orphanagesRepository = getRepository(Orphanage);
-
-  const orphanage = orphanagesRepository.create({
-    name,
-    longitude,
-    latitude,
-    about,
-    instructions,
-    opening_hours,
-    open_on_weekends  
-  });
-
-  await orphanagesRepository.save(orphanage);
-
-  return response.status(201).json(orphanage);
-})
-/* END ROUTES */
+app.use(routes);
 
 app.listen(3333);
