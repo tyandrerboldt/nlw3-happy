e6fa9e099f33db31ce83c6a52695a46ac7c1d832
Tyandrer Boldt <tyandrer.boldt@gmail.com>
2020-10-17 09:46:23 -0300

    Create mobile project

---
+++ mobile/App.tsx
@@ -0,0 +1,66 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, View, Dimensions, Text } from 'react-native';
+import MapView, { PROVIDER_GOOGLE, Marker, Callout } from 'react-native-maps';
+
+import mapMarker from './src/images/map-marker.png';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <MapView 
+      provider={PROVIDER_GOOGLE}
+        style={styles.map} 
+        initialRegion={{
+          latitude: -14.7974657,
+          longitude: -39.0366053,
+          latitudeDelta: 0.008,
+          longitudeDelta: 0.008
+        }}
+      >
+        <Marker
+          icon={mapMarker}
+          calloutAnchor={{
+            x: 2.7,
+            y: 0.8
+          }}
+          coordinate={{
+            latitude: -14.7974657,
+            longitude: -39.0366053,
+          }}
+        >
+          <Callout tooltip>
+            <View style={styles.calloutContainer}>
+              <Text style={styles.calloutText}>Lar das meninas</Text>
+            </View>
+          </Callout>
+        </Marker>
+      </MapView>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+
+  map: {
+    width: Dimensions.get("window").width,
+    height: Dimensions.get("window").height,
+  },
+
+  calloutContainer: {
+    width: 160,
+    height: 46,
+    paddingHorizontal: 16,
+    backgroundColor: 'rgba(255, 255, 255, 0.8)',
+    borderRadius: 16,
+    justifyContent: 'center'
+  },
+
+  calloutText: {
+    color: '#0089a5',
+    fontSize: 14
+  }
+});
