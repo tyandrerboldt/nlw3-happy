ae0777d2975badc3e32384d6828dee5fe2f021df
Tyandrer Boldt <tyandrer.boldt@gmail.com>
2020-10-17 10:28:22 -0300

    Add navigation

---
+++ mobile/src/pages/OrphanageDetails.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text } from 'react-native';
+
+const OrphanageDetails = () => {
+  return (
+    <View>
+      <Text>Detalhes</Text>
+    </View>
+  )
+}
+
+export default OrphanageDetails;
---
+++ mobile/src/routes.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import OrphanagesMap from './pages/OrphanagesMap';
+import OrphanageDetails from './pages/OrphanageDetails';
+
+const { Navigator, Screen } = createStackNavigator();
+
+
+const Routes = () => {
+  return (
+    <NavigationContainer>
+      <Navigator screenOptions={{ headerShown: false }}>
+        <Screen 
+          name="OrphanagesMap" 
+          component={OrphanagesMap} 
+        />
+        <Screen 
+          name="OrphanageDetails" 
+          component={OrphanageDetails} 
+        />
+        {/*
+          OPÇÕES POR TELA
+        <Screen name="OrphanagesMap" component={OrphanagesMap} options={{
+          headerShown: false
+        }} /> */}
+      </Navigator>
+    </NavigationContainer>
+  )
+}
+
+export default Routes;
