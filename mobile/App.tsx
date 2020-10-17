9ec1787c0f4b72687e8b570b8f639f1a30ee6bf8
Tyandrer Boldt <tyandrer.boldt@gmail.com>
2020-10-17 10:06:45 -0300

    Add fonts and home page base layout

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,11 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, View, Dimensions, Text } from 'react-native';
+import { StyleSheet, View, Dimensions, Text, TouchableOpacity } from 'react-native';
 import MapView, { PROVIDER_GOOGLE, Marker, Callout } from 'react-native-maps';
+import { Feather } from '@expo/vector-icons';
+import { useFonts } from 'expo-font';
+import { Nunito_600SemiBold, Nunito_700Bold, Nunito_800ExtraBold } from '@expo-google-fonts/nunito';
 
 import mapMarker from './src/images/map-marker.png';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Nunito_600SemiBold,
+    Nunito_700Bold,
+    Nunito_800ExtraBold
+  });
+
+  if(!fontsLoaded){
+    return null
+  }
+
   return (
     <View style={styles.container}>
       <MapView 
@@ -36,18 +48,49 @@ export default function App() {
           </Callout>
         </Marker>
       </MapView>
+
+      <View style={styles.footer}>
+        <Text style={styles.footerText}>2 orfanatos encontrados</Text>
+        <TouchableOpacity style={styles.createOrphanageButton} onPress={() => {}}>
+          <Feather name="plus" size={20} color="#FFF" />
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
+  createOrphanageButton: {
+    width: 56,
+    height: 56,
+    backgroundColor: "#15c3d6",
+    borderRadius: 20,
+
+    justifyContent: "center",
+    alignItems: "center"    
   },
 
-  map: {
-    width: Dimensions.get("window").width,
-    height: Dimensions.get("window").height,
+  footer: {
+    position: "absolute",
+    left: 24,
+    right: 24,
+    bottom: 32,
+
+    backgroundColor: "#FFF",
+    borderRadius: 20,
+    height: 56,
+    paddingLeft: 24,
+     
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: "center",
+
+    elevation: 4
+  },
+
+  footerText: {
+    fontFamily: 'Nunito_700Bold',
+    color: "#8fa7b3"
   },
 
   calloutContainer: {
@@ -56,11 +99,22 @@ const styles = StyleSheet.create({
     paddingHorizontal: 16,
     backgroundColor: 'rgba(255, 255, 255, 0.8)',
     borderRadius: 16,
-    justifyContent: 'center'
+    justifyContent: 'center',
+    fontFamily: 'Nunito_700Bold'
   },
 
   calloutText: {
     color: '#0089a5',
     fontSize: 14
+  },
+
+  container: {
+    flex: 1,
+  },
+
+  map: {
+    width: Dimensions.get("window").width,
+    height: Dimensions.get("window").height,
   }
+
 });
